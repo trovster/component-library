7a361ef0f0ff3a58b6a2ec4edd1f0074df024b5f
Trevor Morris <code@trovster.com>
2020-04-08 17:54:01 +0100

    feat: blog pages

---
+++ src/components/pages/blog-listing/blog-listing.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  order: 4,
+  title: 'Blog Listing',
+  handle: 'page-blog-listing',
+  status: 'prototype',
+  context: {
+    navigation: {
+      active: 'page-blog-listing'
+    }
+  }
+}
