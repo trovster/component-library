f60aee88039e4d69227a2167c70907a911a5e449
Trevor Morris <code@trovster.com>
2020-04-14 21:46:42 +0100

    fix: tweak to colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,25 +10,44 @@ module.exports = {
 
       white: '#fdfdfd',
       black: '#1d2021',
-      background: '#1d2021',
+
+      background: {
+        default: '#1d2021',
+        dark: '#1d2021',
+        light: '#fdfdfd'
+      },
+
+      text: {
+        default: '#fdfdfd',
+        dark: '#1d2021',
+        light: '#fdfdfd'
+      },
 
       gray: {
-        dark: '#1d2021',
         default: '#333',
+        dark: '#1d2021',
         light: '#666'
       },
 
       blue: {
-        default: '#2dcdff'
+        default: '#2dcdff',
+        dark: '#2dcdff',
+        light: '#2dcdff'
       },
       pink: {
-        default: '#ff1fa5'
+        default: '#ff1fa5',
+        dark: '#ff1fa5',
+        light: '#ff1fa5'
       },
       yellow: {
-        default: '#fff171'
+        default: '#fff171',
+        dark: '#fff171',
+        light: '#fff171'
       },
       green: {
-        default: '#9dff6f'
+        default: '#9dff6f',
+        dark: '#9dff6f',
+        light: '#9dff6f'
       },
 
       primary: '#2dcdff', // theme => theme('colors.blue.default'),
